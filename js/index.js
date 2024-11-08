db6976dddf498540a4212b54987a9bcbb9d39069
Breno Gaebler <brenogaebler@gmail.com>
2024-11-08 15:10:11 -0300

    sos

--- js/index.js
+++ js/index.js
@@ -5,3 +5,14 @@ const year = document.getElementById("year");
 const current_year = new Date().getFullYear();
 year.textContent = current_year;
 
+user_screen_width = window.innerWidth;
+user_screen_height = window.innerHeight;
+
+if (user_screen_width < user_screen_height0){
+    const footer = document.querySelector("footer");
+    const content = document.getElementsByClassName("content")[0];
+
+    footer.classList.add("bug");
+    content.classList.add("bug");
+}
+
